404eb5024ebb29f16f393c598ddfe13ed8924409
StevenKell <steventkell@gmail.com>
2024-09-09 16:49:38 -0400

    See if still works after adding Vuetify.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <img src="./src/assets/images/BetterlookingEBposter (1).png" alt="">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
-
-
 export default {
   name: 'App',
+
   components: {
     HelloWorld,
-    
-  }
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
