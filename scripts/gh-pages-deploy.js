dbe0d6042d036a900b0d4618a44ab6086c33ac82
StevenKell <steventkell@gmail.com>
2024-09-15 21:25:55 -0400

    deploy

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const execa = import("execa");
-const fs = import("fs");
+const fs = ("fs");
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
